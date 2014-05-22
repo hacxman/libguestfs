fa916f10b75808ebeca190b7d1094ef30d5d1592
Maros Zatko <mzatko@fedoraproject.org>
2014-05-22 17:48:33 +0200

    per-handle locking: more utility fns

--- src/locking.c
+++ src/locking.c
@@ -32,9 +32,64 @@
 #include "guestfs-internal-actions.h"
 #include "guestfs_protocol.h"
 
-typeof (per_handle_locks)
-per_handle_lock_add (guestfs_h *g,
-                    typeof(per_handle_locks) locks)
-{
+gl_lock_define_initialized (static, locks_lock);
 
+/* items in handles array bijectively corresponds to items in per_handle_locks
+ */
+static guestfs_h ** handles;
+static size_t n_handles;
+
+void
+guestfs___per_handle_lock_is_not_held (guestfs_h *g)
+{
+  // TODO factor-out for as lookup_handle
+  for (size_t i = 0; i < n_handles; i++) {
+    if (handles[i] == g) {
+      gl_lock_lock(per_handle_locks[i]);
+      gl_lock_unlock(per_handle_locks[i]);
+      return;
+    }
+  }
+  // TODO error in case handle was not found
+}
+
+void
+guestfs___per_handle_lock_add (guestfs_h *g)
+{
+  gl_lock_lock (locks_lock);
+
+  n_handles++;
+  per_handle_locks = realloc (per_handle_locks,
+      sizeof (*per_handle_locks) * n_handles);
+  handles = realloc (handles, sizeof (*handles) * n_handles);
+
+  gl_lock_init (per_handle_locks[n_handles - 1]);
+  handles[n_handles - 1] = g;
+
+  gl_lock_unlock (locks_lock);
+}
+
+void
+guestfs___per_handle_lock_remove (guestfs_h *g)
+{
+  // locks_lock prevents per_handle_locks manipulation within this module
+  gl_lock_lock (locks_lock);
+
+  // ensure we don't get into data race when removing lock
+  guestfs___per_handle_lock_is_not_held (g);
+
+  for (size_t i = 0; i < n_handles; i++) {
+    if (handles[i] == g) {
+      gl_lock_destroy (per_handle_locks[i]);
+      handles[i] = NULL;
+    }
+  }
+
+  n_handles++;
+  per_handle_locks = realloc (per_handle_locks,
+      sizeof (*per_handle_locks) * n_handles);
+  handles = realloc (handles, sizeof (*handles) * n_handles);
+
+
+  gl_lock_unlock (locks_lock);
 }
