42618e98b20b18318956355e4b71155a00e9c84d
Maros Zatko <hacxman@gmail.com>
2014-10-24 15:21:31 +0200

    rl.c: quoting - test single word

--- fish/test/testquoting.c
+++ fish/test/testquoting.c
@@ -67,6 +67,36 @@ test_singlespace_dequote (void)
   }
 }
 
+int
+test_singleword_quote (void)
+{
+  char * q = bsquote_filename("singleword");
+  if (q != NULL && q != -1) {
+    if (STREQ(q, "singleword")) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+
+int
+test_singleword_dequote (void)
+{
+  char * q = debsquote_filename("singleword");
+  if (q != NULL && q != -1) {
+    if (STREQ(q, "singleword")) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+
 struct test_t {
   char *name;
   int (*fn)(void);
@@ -78,6 +108,8 @@ struct test_t tests[] = {
   { .name = "test empty dequote", .fn = test_empty_dequote, .expect = 1},
   { .name = "test single space quote", .fn = test_singlespace_quote, .expect = 1},
   { .name = "test single space dequote", .fn = test_singlespace_dequote, .expect = 1},
+  { .name = "test single word quote", .fn = test_singleword_quote, .expect = 1},
+  { .name = "test single word dequote", .fn = test_singleword_dequote, .expect = 1},
 };
 
 size_t nr_tests = sizeof(tests) / sizeof(*tests);
